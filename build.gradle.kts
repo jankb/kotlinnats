472db52d06c1353cb313491fadb13d109b135f32
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2023-11-22 15:01:53 +0100

    Adde nats lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ dependencies {
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	// https://mvnrepository.com/artifact/io.nats/jnats
+	implementation("io.nats:jnats:2.17.1")
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
